1f5345adde96ef4edd8227fd481f0bffd8f4b22e
justinstern <justin@skyverge.com>
2017-06-10 23:50:40 -0400

    Fix dev version

--- woocommerce-dev-helper.php
+++ woocommerce-dev-helper.php
@@ -5,7 +5,7 @@
  * Description: A simple plugin for helping develop/debug WooCommerce & extensions
  * Author: SkyVerge
  * Author URI: http://www.skyverge.com
- * Version: 0.7.0-dev
+ * Version: 0.7.1-dev
  * Text Domain: woocommerce-dev-helper
  * Domain Path: /i18n/languages/
  *
