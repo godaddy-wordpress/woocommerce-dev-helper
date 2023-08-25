f9cc4830c6b8e6a9e6cdfe7436627b19c81e0521
nikolas4175-godaddy <nikolas4175@godaddy.com>
2023-08-25 15:30:31 -0700

    define block gateway JS

---
+++ includes/block-checkout.js
@@ -0,0 +1,23 @@
+const settings = window.wc.wcSettings.getSetting( 'bogus_data', {} );
+
+const label = window.wp.htmlEntities.decodeEntities( settings.title ) || window.wp.i18n.__( 'Bogus', 'woocommerce-dev-helper' );
+
+const Content = () => {
+	console.log('SV DEBUG: loading bogus gateway block content');
+	return window.wp.htmlEntities.decodeEntities( settings.description || '' );
+};
+
+const BogusGateway = {
+	name: 'bogus_gateway',
+	label: label,
+	content: Object( window.wp.element.createElement )( Content, null ),
+	edit: Object( window.wp.element.createElement )( Content, null ),
+	canMakePayment: () => true,
+	placeOrderButtonLabel: window.wp.i18n.__( 'Continue', 'woocommerce-dev-helper' ),
+	ariaLabel: label,
+	supports: {
+		features: settings.supports,
+	},
+};
+
+window.wc.wcBlocksRegistry.registerPaymentMethod( BogusGateway );
