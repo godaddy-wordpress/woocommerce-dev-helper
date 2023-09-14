f8a4a23b2fd0644045c10a8b88c8dcf492080471
nikolas4175-godaddy <nikolas4175@godaddy.com>
2023-09-14 15:13:53 -0700

    submit field input for gateway processing

--- client/js/blocks/block-checkout.js
+++ client/js/blocks/block-checkout.js
@@ -1,8 +1,8 @@
 import { registerPaymentMethod } from '@woocommerce/blocks-registry';
 import { __ } from '@wordpress/i18n';
-import { createElement, useState } from '@wordpress/element';
+import { createElement, useState, useEffect } from '@wordpress/element';
 import { TextInput } from '@woocommerce/blocks-checkout';
-import { SelectControl } from 'wordpress-components';
+// import { SelectControl } from 'wordpress-components';
 import { decodeEntities } from '@wordpress/html-entities';
 import { getSetting } from '@woocommerce/settings';
 
@@ -12,15 +12,56 @@ const label = decodeEntities(settings.title) || __('Bogus', 'woocommerce-dev-hel
 
 // const useState = wp.element.useState;
 const elementId = 'wc-blocks-payment-gateways-bogus-gateway-content';
+const SelectControl = window.wp.components.SelectControl;
 
 const Description = () => {
 	return decodeEntities(settings.description || '');
 };
 
-const Content = () => {
+const Content = ( props ) => {
 	const [result, setResult] = useState('approved');
-
 	const options = settings.result_options
+
+	const { eventRegistration, emitResponse } = props;
+	const { onPaymentProcessing } = eventRegistration;
+
+	useEffect( () => {
+		const unsubscribe = onPaymentProcessing( async () => {
+
+			// validate input
+			let inputIsValid = false;
+			for ( const index of options ){
+				inputIsValid = ( index['value'] === result );
+				if (inputIsValid == true) break;
+			}
+
+			// return result for server-side gateway processing
+			if ( inputIsValid ) {
+				return {
+					type: emitResponse.responseTypes.SUCCESS,
+					meta: {
+						paymentMethodData: {
+							'bogus_gateway_payment_result': result
+						},
+					},
+				};
+			}
+
+			return {
+				type: emitResponse.responseTypes.ERROR,
+				message: 'Invalid input. Please use one of the options provided.',
+			};
+		} );
+		// Unsubscribes when this component is unmounted.
+		return () => {
+			unsubscribe();
+		};
+	}, [
+		emitResponse.responseTypes.ERROR,
+		emitResponse.responseTypes.SUCCESS,
+		onPaymentProcessing,
+		result,
+	] );
 
 	return (
 		<div id={elementId}>
